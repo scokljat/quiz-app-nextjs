752cfb5df7c9535df3380035b91dbd2556b1b85e
scokljat <selma.cokljat@hotmail.com>
2023-06-03 21:20:32 +0200

    Added store with action and reducer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
 import { Roboto } from "next/font/google";
 import NavBar from "@/components/NavBar";
+import store from "./store/store";
 import "./globals.css";
 
 const roboto = Roboto({
@@ -11,13 +16,19 @@ export const metadata = {
   title: "Quiz App",
 };
 
+const queryClient = new QueryClient();
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>
-        <NavBar />
-        {children}
-      </body>
-    </html>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <html lang="en">
+          <body className={roboto.className}>
+            <NavBar />
+            {children}
+          </body>
+        </html>
+      </QueryClientProvider>
+    </Provider>
   );
 }
--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,6 +1,17 @@
-import Emoji from "../../../public/bx-happy-beaming.svg";
+"use client";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useSelector, useDispatch } from "react-redux";
+import Emoji from "../../../public/bx-happy-beaming.svg";
+import { updateScore } from "../store/userSlice";
+
 function Page() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { score } = useSelector((state) => {
+    return state;
+  });
+
   return (
     <div className="flex justify-center items-center h-[92vh]">
       <div className="flex flex-col gap-20 justify-center items-center bg-neutral-800 w-[40%] h-[60%] ">
@@ -8,11 +19,19 @@ function Page() {
           <div className="flex flex-col justify-center items-center gap-3">
             <p className="text-[45px] text-[#79a8ce]">Congulations!</p>
             <p className="text-[25px]">YOUR SCORE</p>
-            <p className="text-[45px]">4/5</p>
+            <p className="text-[45px]">{score}/5</p>
           </div>
-          <Image src={Emoji} />
+          <Image src={Emoji} alt="" priority />
         </div>
-        <button className="bg-[#79a8ce] p-2">Try again</button>
+        <button
+          className="bg-[#79a8ce] p-2"
+          onClick={() => {
+            router.push("/quiz");
+            dispatch(updateScore("resetScore"));
+          }}
+        >
+          Try again
+        </button>
       </div>
     </div>
   );
