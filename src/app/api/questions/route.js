2d32bf76b44aa61fefae764588481eda76ae5922
scokljat <selma.cokljat@hotmail.com>
2023-07-03 21:21:52 +0200

    Implemented get method for questions

--- src/app/api/questions/route.js
+++ src/app/api/questions/route.js
@@ -3,8 +3,13 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const data = await prisma.questions.findMany();
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const startIndex = searchParams.get("start");
+  const endIndex = searchParams.get("end");
 
-  return NextResponse.json(data);
+  const allQuestions = await prisma.questions.findMany();
+  const sectionedQuestions = allQuestions.slice(startIndex, endIndex);
+
+  return NextResponse.json(sectionedQuestions);
 }
