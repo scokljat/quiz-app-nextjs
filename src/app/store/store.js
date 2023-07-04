563c36a75a1d932b9038e2d1cf5f6bc64c4eab86
scokljat <selma.cokljat@hotmail.com>
2023-07-04 14:30:51 +0200

    Deleted redux files

--- src/app/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import devToolsEnhancer from "remote-redux-devtools";
-import reducer from "./userSlice";
-
-const store = configureStore({
-  reducer,
-  devTools: [devToolsEnhancer({ realtime: true })],
-});
-export default store;
