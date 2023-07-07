c911f3c2b67934a1fe49abdbc8a41acc5ae0900f
scokljat <selma.cokljat@hotmail.com>
2023-07-07 14:56:27 +0200

    Added logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import { ApiContext } from "../app/context";
+
 function NavBar() {
+  const { token, setToken } = useContext(ApiContext);
+
   return (
-    <div className="w-full bg-neutral-800 h-14 flex justify-center items-center">
+    <div className="w-full bg-neutral-800 h-14 flex justify-between items-center px-4">
       <p className="text-2xl">Quiz app</p>
+      {token && (
+        <button
+          className=" bg-[#79a8ce] p-1 text-[12px]"
+          onClick={() => {
+            setToken("");
+            localStorage.removeItem("token");
+          }}
+        >
+          Log out
+        </button>
+      )}
     </div>
   );
 }
